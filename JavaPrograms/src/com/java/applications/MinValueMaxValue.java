365524c84b3ac697e91df0bc2c6dfaebae44fecc
ry <ry@rys-macbook-pro>
2019-08-29 22:31:26 -0500

    Min max program

---
+++ JavaPrograms/src/com/java/applications/MinValueMaxValue.java
@@ -0,0 +1,27 @@
+package com.java.applications;
+
+public class MinValueMaxValue {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		/*
+		   To identify "min value" and "max value" for each and every data type, JAVA has provided the
+		   following two constant variables from all the wrapper classes.
+           MIN_VALUE and MAX_VALUE
+		 */
+		
+		System.out.println(Byte.MIN_VALUE+"----->"+Byte.MAX_VALUE); 
+		System.out.println(Short.MIN_VALUE+"---->"+Short.MAX_VALUE); 
+		System.out.println(Integer.MIN_VALUE+"----->"+Integer.MAX_VALUE); 
+		System.out.println(Long.MIN_VALUE+"----->"+Long.MAX_VALUE); 
+		System.out.println(Float.MIN_VALUE+"----->"+Float.MAX_VALUE); 
+		System.out.println(Double.MIN_VALUE+"----->"+Double.MAX_VALUE); 
+		System.out.println(Character.MIN_VALUE+"----->"+Character.MAX_VALUE); 
+		// System.out.println(Boolean.MIN_VALUE+"----->"+Boolean.MAX_VALUE);---> Error
+		
+		
+	}
+
+}
