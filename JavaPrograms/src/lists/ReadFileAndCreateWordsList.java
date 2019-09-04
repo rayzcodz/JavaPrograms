313883fd862ce36d3eada1adc6b73e901ae06c61
ry <ry@rys-macbook-pro>
2019-09-03 23:21:40 -0500

    Read a text file and create a List containing the words inside the file.

---
+++ JavaPrograms/src/lists/ReadFileAndCreateWordsList.java
@@ -0,0 +1,31 @@
+package lists;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReadFileAndCreateWordsList {
+
+	public static void main(String[] args) {
+		Scanner scan = null;
+		try {
+			scan = new Scanner(new File("/Users/ry/git/JavaPrograms/JavaPrograms/src/lists/textfile.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		ArrayList<String> words = new ArrayList<String>();
+		
+		while (scan.hasNext()) {
+			String word = scan.next();
+			words.add(word);
+		}
+		
+		System.out.println(words);
+		
+		
+
+	}
+
+}
