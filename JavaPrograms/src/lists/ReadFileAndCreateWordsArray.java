15c3516055aeb0115642680d6301003e12bd935b
ry <ry@rys-macbook-pro>
2019-09-03 23:21:59 -0500

    Name change/update.

---
+++ JavaPrograms/src/lists/ReadFileAndCreateWordsArray.java
@@ -0,0 +1,30 @@
+package lists;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+ * We are reading a file and adding each word from the file into a list.
+ */
+
+public class ReadFileAndCreateWordsArray {
+	public static void main(String ... ray) throws FileNotFoundException {
+		
+		Scanner scan = new Scanner(new File("/Users/ry/git/JavaPrograms/JavaPrograms/src/lists/textfile.txt"));
+		
+		String[] words = new String[10]; //Making size = 10 because there 10 words in the file.
+		int index = 0;
+		
+		while (scan.hasNext()) {
+			String word = scan.next();
+			words[index] = word;
+			index += 1;
+		}
+		
+		System.out.println(Arrays.toString(words));
+		
+	}
+
+}
