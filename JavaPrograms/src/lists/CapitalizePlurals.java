1bc476746b8657d9488e8b70e4eb119ab26fd3b3
ry <ry@rys-macbook-pro>
2019-09-05 08:37:38 -0500

    Capitalize plurals.

---
+++ JavaPrograms/src/lists/CapitalizePlurals.java
@@ -0,0 +1,34 @@
+package lists;
+
+import java.util.ArrayList;
+
+public class CapitalizePlurals {
+	
+	static void capitalizePlurals(ArrayList<String> words) {
+		
+		for(int i = 0; i < words.size(); i++ ) {
+			if(words.get(i).endsWith("s")) {
+				words.set(i, words.get(i).substring(0, 1).toUpperCase()+words.get(i).substring(1));
+
+			}
+		}
+		
+		System.out.println(words);
+		
+	}
+
+	
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<>();
+		list.add("anas");
+		list.add("anad");
+		list.add("nana");
+		list.add("nanas");
+		list.add("manas");
+		list.add("anat");
+		
+		capitalizePlurals(list);
+		
+		
+	}
+}
