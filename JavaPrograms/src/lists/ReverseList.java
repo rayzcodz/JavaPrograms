93e1bb2eb3775420f180f99780c652431cd43220
ry <ry@rys-macbook-pro>
2019-09-04 19:59:15 -0500

    Create a method to reverse words in a list.

---
+++ JavaPrograms/src/lists/ReverseList.java
@@ -0,0 +1,30 @@
+package lists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ReverseList {
+	
+	
+	public ArrayList<String> reverse(ArrayList<String> words) {
+		ArrayList<String> reversedWords = new ArrayList<>();
+		
+		for(int i = words.size() - 1; i >= 0; i--) {
+			reversedWords.add(words.get(i));
+		}
+		
+		return reversedWords;
+		
+	}
+	
+
+	static public void main(String something []) {
+		
+		ReverseList rl = new ReverseList(); //we have to create this object so that we can use above method
+		
+		ArrayList<String> wordsList = new ArrayList<String>(Arrays.asList("Anna", "Manna", "Xasha", "Kola", "Zona"));//this is the list we will reverse
+		ArrayList<String> result = rl.reverse(wordsList);
+		System.out.println(result);
+		
+	}
+}
