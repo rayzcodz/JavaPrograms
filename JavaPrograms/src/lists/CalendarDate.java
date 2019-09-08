eeabc24b0a8d1d6936a7c9a75828104172af3bba
ry <ry@rys-macbook-pro>
2019-09-07 20:12:18 -0500

    Create custom method to compare dates.

---
+++ JavaPrograms/src/lists/CalendarDate.java
@@ -0,0 +1,50 @@
+package lists;
+
+/*
+ * Our goal is to list birthdays (M, D i.e 5, 1; 10, 2; 3, 4) format 
+ * in ascending order. In order to do that We will create a CalendarDate class
+ * which implements Comparable interface, and which implements the compareTo method of the 
+ * Comparable interface.
+ * 
+ */
+
+
+public class CalendarDate implements Comparable<CalendarDate> {
+
+	private int month;
+	private int day;
+	
+	
+	public CalendarDate(int month, int day) {
+		this.month = month;
+		this.day = day;
+	}
+	
+	
+	
+	
+	// Compares this calendar date to another date.
+	// Dates are compared by month and then by day.
+	@Override
+	public int compareTo(CalendarDate o) {
+		if(month != o.month) {
+			return month - o.month;
+		} else {
+			return day - o.day;
+		}
+	}
+	
+	public int getMonth() {
+		return month;
+	}
+	
+	public int getDay() {
+		return day;
+	}
+	
+	@Override
+	public String toString() {
+		return month + "/" + day;
+	}
+
+}
---
+++ JavaPrograms/src/lists/CalendarDateTest.java
@@ -0,0 +1,21 @@
+package lists;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CalendarDateTest {
+
+	public static void main(String[] args) {
+		ArrayList<CalendarDate> dates = new ArrayList<CalendarDate>();
+		dates.add(new CalendarDate(2, 22));
+		dates.add(new CalendarDate(10, 30));
+		dates.add(new CalendarDate(4, 13));
+		dates.add(new CalendarDate(3, 16));
+		dates.add(new CalendarDate(4, 28));
+		
+		System.out.println("Dates before sorting = " + dates);
+		Collections.sort(dates);
+		System.out.println("Dates after sorting = " + dates);
+
+	}
+}
